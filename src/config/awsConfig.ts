85053180d1d49c7f790f1c2ce5b3cf42d025f618
RKRafaelNascimento <36175807+RKRafaelNascimento@users.noreply.github.com>
2025-01-31 23:25:36 -0300

    feat: add AWS configuration for region and credentials

---
+++ src/config/awsConfig.ts
@@ -0,0 +1,18 @@
+interface IAWSVariable {
+  region: string;
+  credentials: {
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+}
+
+export const awsConfig = (): IAWSVariable => ({
+  region: process.env.AWS_REGION || "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+export const awsBucketExpenseBatch =
+  process.env.AWS_BUCKET_EXPENSE_BATCH || "expense-batch";
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
 export { default as applicationConfig } from "./application";
 export { loggerConfig } from "./loggerConfig";
+export { awsConfig, awsBucketExpenseBatch } from "./awsConfig";
