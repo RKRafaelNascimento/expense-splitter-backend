da9febe30c3dc81d232b8f144049b75fe0bf1623
RKRafaelNascimento <36175807+RKRafaelNascimento@users.noreply.github.com>
2025-02-02 20:25:33 -0300

    fix: use factory functions for dependencies in BalanceService tests

--- src/modules/balance/__test__/balance.spec.ts
+++ src/modules/balance/__test__/balance.spec.ts
@@ -52,8 +52,8 @@ describe("BalanceService", () => {
 
     balanceService = new BalanceService(
       mockBalanceRepository,
-      mockGroupMemberService,
-      mockExpenseService,
+      () => mockGroupMemberService,
+      () => mockExpenseService,
     );
   });
 
